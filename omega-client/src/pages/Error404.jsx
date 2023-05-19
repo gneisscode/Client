7d8d9b54e13bfea2c165ea93bc911ac9be337e40
Marvelous <Marvelous@DESKTOP-51HEQCN.com>
2023-05-20 00:54:43 +0100

    Worked on error-404 page

--- omega-client/src/pages/Error404.jsx
+++ omega-client/src/pages/Error404.jsx
@@ -5,7 +5,14 @@ const Error404 = () => {
   return (
     <div>
       <HomeNav/>
-      Error404
+      <div className='flex justify-center mt-8'>
+        <img src="assets/error/error.png" alt='error' className='w-1/3'/>
+      </div>
+      <div className='text-center mt-10 leading-4 text-base'>
+        <p>We are sorry, the page you are looking for is not available.</p>
+        <p>Perhaps you can try a new search.</p>
+        <button className='bg-blue-600 mt-24 px-36 py-2 text-white rounded-sm hover:border-purple-500 hover:border-2'>Go Back</button>
+      </div>
       </div>
   )
 }
