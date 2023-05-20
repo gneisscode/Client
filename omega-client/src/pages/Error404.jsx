8fc052da07995e9b19d5f3dd900052d910e69358
gneisscode <103408439+gneisscode@users.noreply.github.com>
2023-05-20 09:49:34 +0200

    edited 404 page

--- omega-client/src/pages/Error404.jsx
+++ omega-client/src/pages/Error404.jsx
@@ -8,10 +8,10 @@ const Error404 = () => {
       <div className='flex justify-center mt-8'>
         <img src="assets/error/error.png" alt='error' className='w-1/3'/>
       </div>
-      <div className='text-center mt-10 leading-4 text-base'>
+      <div className=' flex flex-col gap-4 items-center text-center mt-10 leading-4 text-base'>
         <p>We are sorry, the page you are looking for is not available.</p>
         <p>Perhaps you can try a new search.</p>
-        <button className='bg-blue-600 mt-24 px-36 py-2 text-white rounded-sm hover:border-purple-500 hover:border-2'>Go Back</button>
+        <button className='bg-blue-600 mt-20 px-36 py-2 text-white w-[589px] h-[61px] rounded-[4px] hover:border-purple-500 hover:border-2 mb-16'>Go Back</button>
       </div>
       </div>
   )
