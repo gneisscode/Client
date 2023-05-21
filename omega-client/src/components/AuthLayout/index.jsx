8b35f50084730359a81cbaeb7bd5fc8cde85361b
EllaLawrie <lawrieella002@gmail.com>
2023-05-21 21:33:58 +0100

    updated changes

---
+++ omega-client/src/components/AuthLayout/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AuthLayout = ({ children }) => {
+  return (
+    <div className='grid lg:grid-cols-2 grid-cols-1'>
+      <section className='bg-[#012966]'>{children}</section>
+      <section className='hidden lg:block'>
+        <div className='sectionBg h-screen'></div>
+      </section>
+    </div>
+  )
+}
+
+export default AuthLayout
