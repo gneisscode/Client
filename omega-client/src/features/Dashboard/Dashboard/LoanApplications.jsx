b1021342c50901d82b9b0114c3d3a73ec472ee03
gneisscode <103408439+gneisscode@users.noreply.github.com>
2023-05-23 09:48:21 +0200

    added dummy data for loan applications

--- omega-client/src/features/Dashboard/Dashboard/LoanApplications.jsx
+++ omega-client/src/features/Dashboard/Dashboard/LoanApplications.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DashHeader from "../../../components/Dashboard/DashHeader";
 import Sidebar from "../../../components/Dashboard/Sidebar";
 
 
 const LoanApplications = () => {
+    const [data, SetData] = useState([
+      {
+        "Borrowers Name": "Blessing Effiong",
+        Date: "15/02/23",
+        Status: "Successful",
+        "Credit Score": 520,
+        Amount: "#5,000,000",
+      },
+
+      {
+        "Borrowers Name": "Blessing Effiong",
+        Date: "15/02/23",
+        Status: "Pending",
+        "Credit Score": 520,
+        Amount: "#5,000,000",
+      },
+
+      {
+        "Borrowers Name": "Blessing Effiong",
+        Date: "15/02/23",
+        Status: "Declined",
+        "Credit Score": 520,
+        Amount: "#5,000,000",
+      },
+    ]);
+
   return (
     <div className="flex flex-col">
       <DashHeader />
