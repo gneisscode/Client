562d9c131becda4b6b859eb34a94a6700be164e3
EllaLawrie <lawrieella002@gmail.com>
2023-05-24 02:22:36 +0100

    update made

--- omega-client/src/components/AuthLayout/index.jsx
+++ omega-client/src/components/AuthLayout/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AuthLayout = ({ children }) => {
   return (
     <div className='grid lg:grid-cols-2 grid-cols-1'>
-      <section className='bg-[#012966]'>{children}</section>
+      <section className='bg-[#012966] h-full'>{children}</section>
       <section className='hidden lg:block'>
         <div className='sectionBg h-screen'></div>
       </section>
