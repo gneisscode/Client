c248c8f9b176fe0bfda2232fb10117be7e3e323f
EllaLawrie <lawrieella002@gmail.com>
2023-05-25 22:22:01 +0100

    Faq completed

--- omega-client/src/features/Dashboard/HelpSupport/Faq.jsx
+++ omega-client/src/features/Dashboard/HelpSupport/Faq.jsx
@@ -5,43 +5,60 @@ const Faq = () => {
     {
       question: 'How can i determine a loan default?',
       answer:
-        'Loan default can be determined when the borrowers is  financially incapable to pay back the loan. This can be because of his/her loan credit score or past history of loan defaulting. If you need further assistance ask a team member ',
+        'Loan default can be determined when the borrowers is  financially incapable to pay back the loan. This can be because of his/her loan credit score or past history of loan defaulting. If you need further assistance ask a team member.',
     },
     {
-      question: 'Financial advice to be given to borrower',
-      answer: 'Answer 2',
+      question: 'Financial advice to be given to borrower?',
+      answer:
+        'Loan default can be determined when the borrowers is  financially incapable to pay back the loan. This can be because of his/her loan credit score or past history of loan defaulting. If you need further assistance ask a team member.',
     },
     {
-      question: 'What type of loans are available',
-      answer: 'Answer 3',
+      question: 'What type of loans are available?',
+      answer:
+        'Loan default can be determined when the borrowers is  financially incapable to pay back the loan. This can be because of his/her loan credit score or past history of loan defaulting. If you need further assistance ask a team member.',
     },
     {
-      question: 'Special loan offers for team members',
-      answer: 'Answer 4',
+      question: 'Special loan offers for team members?',
+      answer:
+        'Loan default can be determined when the borrowers is  financially incapable to pay back the loan. This can be because of his/her loan credit score or past history of loan defaulting. If you need further assistance ask a team member.',
     },
   ]
 
-  const [expandedItems, setExpandedItems] = useState(faqItems)
-
-  const toggleItem = (index) => {
-    setExpandedItems((prevState) => ({
-      expandedItems: {
-        ...prevState.expandedItems,
-        [index]: !prevState.expandedItems[index],
-      },
-    }))
+  const [currentQ, setCurrentQ] = useState(null)
+  const [toggle, setToggle] = useState(true)
+  const toggleItem = (idx) => {
+    setToggle((prevState) => {
+      if (prevState === false) {
+        setCurrentQ(idx)
+        return !toggle
+      } else {
+        setCurrentQ(null)
+        return !toggle
+      }
+    })
   }
 
   return (
-    <div className='mt-8 px-8'>
+    <div className='mt-10 px-8'>
       <div>
-        {expandedItems.map((item, index) => (
-          <div key={index} className='bg-white px-4 border border-inherit'>
-            <div onClick={() => toggleItem(index)} className='cursor-pointer'>
+        {faqItems.map((item, index) => (
+          <div key={index} className='bg-white text-[#666666]'>
+            <div
+              onClick={() => toggleItem(index)}
+              className='cursor-pointer border hover:ease-in hover:bg-[#FAFCFF] px-6 py-6 font-medium '
+            >
               {item.question}
             </div>
 
-            <div>{item.answer}</div>
+            <div
+              className={
+                currentQ === index
+                  ? 'px-6 py-2 font-normal text-[#949292]'
+                  : 'hidden'
+              }
+            >
+              {item.answer}
+            </div>
           </div>
         ))}
       </div>
