f6699e40e509a88aca3800f12f70649a1ae0f071
Ketcha KAREN <60319455+leonelta@users.noreply.github.com>
2023-05-31 05:09:01 +0100

    Update index.jsx

--- omega-client/src/features/Auth/ForgotPassword/index.jsx
+++ omega-client/src/features/Auth/ForgotPassword/index.jsx
@@ -8,9 +8,14 @@ const ForgotPassword = () => {
   return (
     <div className=" flex flex-col justify-center items-center pt-[68px] pb-[2em] gap-8">
       <LockIcon />
+      <h1 className="ml-[2em] mt-[2em]">Forgot Password</h1>
+      <p className="text-xl pb-4">kindly insert your email below, a verification code will be sent to you
+        , make sure you enter the correct email
+      </p>
       <Card className= "h-[506px]">
-        <div className="ml-[2em] mt-[2em]">Forgot Password</div>
+        <input className="border border-blue-600 w-[589px] h-[61px] p-6" placeholder="Email Address:"/>
         <PasswordBtn text="Send" className="ml-[2em] mt-[2em]" />
+        <p className="text-xl text-blue ml-[2em] mt-[2em]"><Link to='/login'>Back to Sign In</Link></p>
       </Card>
     </div>
   );
