31aa9d3e31c08cc2db109b179a92d6a8eb775c23
StephOge <nwachukwustephenie0@gmail.com>
2023-06-03 18:15:36 +0100

    loans-corrected

--- omega-client/src/features/Dashboard/Dashboard/loans/Generated.jsx
+++ omega-client/src/features/Dashboard/Dashboard/loans/Generated.jsx
@@ -30,14 +30,14 @@ const Generated = () => {
         <div className="lg:w-[938px] lg:h-[fit] bg-[#FAFCFF] border border-[#CCE1FF] ml-[44px] mt-[128px] mb-16">
           <div className="flex justify-between">
           <div className="mt-5 p-5">
-            <p className="font-semibold text-xl text-blue-800 ">Loans Generated</p>
-            <p className="font-medium text-base text-blue-500 mt-5">Total Amount: 4,587,541.28</p>
-            <p className="font-semibold text-base text-blue-700 mt-5">Last Month</p>
+            <p className="font-semibold text-xl text-[#013E99] ">Loans Generated</p>
+            <p className="font-medium text-base text-[#0267FF] mt-5">Total Amount: 4,587,541.28</p>
+            <p className="font-semibold text-base text-[#0252CC] mt-5">Last Month</p>
           </div>
           <div className="flex mt-10 p-10 mr-10 gap-2 items-center">
-            <p className="text-blue-500">Sort by</p>
+            <p className="text-[#0252CC]">Sort by</p>
             <TfiAngleDown />
-            <p className="text-gray-700">Month</p>
+            <p className="text-[#4D4D4D]">Month</p>
           </div>
           </div>
 
@@ -45,23 +45,23 @@ const Generated = () => {
             <table className="min-w-full table-fixed border-collapse">
               <thead >
                 <tr>
-                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-gray-700">Borrower's Name</th>
-                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-gray-700">Date</th>
-                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-gray-700">Status</th>
-                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-gray-700">Credit Score</th>
-                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-gray-700">Amount</th>
+                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">Borrower's Name</th>
+                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">Date</th>
+                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">Status</th>
+                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">Credit Score</th>
+                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">Amount</th>
                 </tr>
               </thead>
               <tbody>
                 {repeatData.map((row, index) => (
                   <tr key={index} className={index % 2 === 0 ? 'bg-gray-50' : 'bg-white'}>
-                    <td className="px-6 py-4 font-semibold text-base text-gray-700">{row.name}</td>
-                    <td className="px-6 py-4 font-semibold text-base text-gray-700">{row.date}</td>
-                    <td className="px-6 py-4 font-semibold text-base text-gray-700">
-                      <span className={`text-blue-700`}>{row.status}</span>
+                    <td className="px-6 py-4 font-semibold text-base text-[#666666]">{row.name}</td>
+                    <td className="px-6 py-4 font-semibold text-base text-[#666666]">{row.date}</td>
+                    <td className="px-6 py-4 font-semibold text-base text-[#666666]">
+                      <span className={`text-[#0267FF]`}>{row.status}</span>
                     </td>
-                    <td className="px-6 py-4 font-semibold text-base text-gray-700">{row.creditScore}</td>
-                    <td className="px-6 py-4 font-semibold text-base text-gray-700">{row.amount}</td>
+                    <td className="px-6 py-4 font-semibold text-base text-[#666666]">{row.creditScore}</td>
+                    <td className="px-6 py-4 font-semibold text-base text-[#666666]">{row.amount}</td>
                   </tr>
                 ))}
               </tbody>
