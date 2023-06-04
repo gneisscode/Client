2a0ad8ca1b5bc4ff473be9389d856a42125be2f2
Ketcha KAREN <60319455+leonelta@users.noreply.github.com>
2023-06-04 23:23:18 +0100

    Update BorrowersSaved.jsx

--- omega-client/src/features/Dashboard/SavedData/BorrowersSaved.jsx
+++ omega-client/src/features/Dashboard/SavedData/BorrowersSaved.jsx
@@ -37,28 +37,42 @@ const BorrowersSaved = () => {
         <DashHeader />
         <div className="flex relative">
             <Sidebar />
-            <div className="grid grid-cols-2 justify-between items-center gap-20 mt-16 pb-[147px]">
-            {activeIndex !== 0 ? (
-                <Button
-                  className="bg-white text-[#0267FF] border border-[#0267FF] w-4/12"
-                  label="Previous"
-                  onClick={() => setActiveIndex((prev) => prev - 1)}
-                />
-              ) : (
-                <div className="w-4/12"></div>
-              )}
-              <Button
-                    className={`text-white bg-[#0267FF] ${
-                    activeIndex === 0 ? "w-4/12" : "w-4/12"
-                    }`}
-                    label={activeIndex === 3 ? "Save Data" : "Next"}
-                    onClick={() =>
-                    activeIndex === 3
-                        ? setModal(true)
-                        : setActiveIndex((prev) => prev + 1)
-                    }
-                />
-        </div>
+            <section>
+              <div>
+                <div className="flex flex-col gap-[16px]">
+                  <h3 className="text-[#0267FF] text-[24px] font-[600]">
+                     Borrower's Saved Data
+                  </h3>
+                  <h4 className="text-[#4D4D4D] text-[20px] font-[600]">
+                    {step.title}
+                  </h4>
+                </div>
+                <div className="flex w-full mt-16">
+                  {step.form}
+                </div>
+                <div className="grid grid-cols-2 justify-between items-center gap-20 mt-16 pb-[147px]">
+                  {activeIndex !== 0 ? (
+                      <Button
+                        className="bg-white text-[#0267FF] border border-[#0267FF] w-4/12"
+                        label="Previous"
+                        onClick={() => setActiveIndex((prev) => prev - 1)}
+                      />
+                    ) : (
+                      <div className="w-4/12"></div>
+                    )}
+                  <Button
+                        className={`text-white bg-[#0267FF] ${
+                        activeIndex === 0 ? "w-4/12" : "w-4/12"
+                        }`}
+                        label={activeIndex === 3 ? "Ok" : "Next"}
+                        onClick={() =>
+                        activeIndex === 3 
+                            : setActiveIndex((prev) => prev + 1)
+                        }
+                  />
+                </div>
+              </div>
+            </section>
         </div>
     </div>
   )
