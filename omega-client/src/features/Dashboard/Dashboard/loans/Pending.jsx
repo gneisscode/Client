e0315cbb54446f4995b1bb2e43fa02c2cdfead49
StephOge <nwachukwustephenie0@gmail.com>
2023-06-04 21:57:08 +0100

    resolved

--- omega-client/src/features/Dashboard/Dashboard/loans/Pending.jsx
+++ omega-client/src/features/Dashboard/Dashboard/loans/Pending.jsx
@@ -26,45 +26,48 @@ const Pending = () => {
   return (
     <div className="flex flex-col">
       <DashHeader />
-      <div className="flex relative ">
+      <div className="flex relative">
         <Sidebar />
         <div className="lg:w-[938px] lg:h-[fit] bg-[#FAFCFF] border border-[#CCE1FF] ml-[44px] mt-[128px] mb-16">
           <div className="flex justify-between">
-          <div className="mt-5 p-5">
-            <p className="font-semibold text-xl text-[#013E99]  ">Loans Pending</p>
-            <p className="font-medium text-base text-[#F29509] mt-5">Total Amount: 4,587,541.28</p>
-            <p className="font-semibold text-base text-[#0252CC] mt-5">Last Month</p>
+            <div className="mt-5 p-5">
+              <p className="font-semibold text-xl text-[#013E99] ">
+                Loans Generated
+              </p>
+              <p className="font-medium text-base text-[#0267FF] mt-5">
+                Total Amount: 4,587,541.28
+              </p>
+              <p className="font-semibold text-base text-[#0252CC] mt-5">
+                Last Month
+              </p>
+            </div>
+            <div className="flex mt-10 p-10 mr-10 gap-2 items-center">
+              <p className="text-[#0252CC]">Sort by</p>
+              <TfiAngleDown />
+              <p className="text-[#4D4D4D]">Month</p>
+            </div>
           </div>
-          <div className="flex mt-10 p-10 mr-10 gap-2 items-center">
-            <p className="text-[#0252CC]">Sort by</p>
-            <TfiAngleDown />
-            <p className="text-[#4D4D4D]">Month</p>
-          </div>
-          </div>
+          <div className="lg:w-[938px] lg:h-[fit] bg-[#FAFCFF] border border-[#CCE1FF] mt-[40px] mb-16">
 
-          <div className="overflow-x-auto flex items-center justify-center">
-            <table className="min-w-full table-fixed border-collapse">
-              <thead >
-                <tr>
-                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">Borrower's Name</th>
-                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">Date</th>
-                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">Status</th>
-                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">Credit Score</th>
-                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">Amount</th>
-
-                </tr>
-              </thead>
-              <tbody>
-                {repeatData.map((row, index) => (
-                  <tr key={index} className={index % 2 === 0 ? 'bg-gray-50' : 'bg-white'}>
-                    <td className="px-6 py-4 font-semibold text-base text-[#666666]">{row.name}</td>
-                    <td className="px-6 py-4 font-semibold text-base text-[#666666]">{row.date}</td>
-                    <td className="px-6 py-4 font-semibold text-base text-[#666666]">
-                      <span className={`text-[#F29509]`}>{row.status}</span>
-                    </td>
-                    <td className="px-6 py-4 font-semibold text-base text-[#666666]">{row.creditScore}</td>
-                    <td className="px-6 py-4 font-semibold text-base text-[#666666]">{row.amount}</td>
-
+            <div className="overflow-x-auto flex items-center justify-center">
+              <table className="min-w-full table-fixed border-collapse">
+                <thead>
+                  <tr>
+                    <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">
+                      Borrower's Name
+                    </th>
+                    <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">
+                      Date
+                    </th>
+                    <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">
+                      Status
+                    </th>
+                    <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">
+                      Credit Score
+                    </th>
+                    <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">
+                      Amount
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
@@ -73,19 +76,19 @@ const Pending = () => {
                       key={index}
                       className={index % 2 === 0 ? "bg-gray-50" : "bg-white"}
                     >
-                      <td className="px-6 py-4 font-semibold text-base text-gray-700">
+                      <td className="px-6 py-4 font-semibold text-base text-[#666666]">
                         {row.name}
                       </td>
-                      <td className="px-6 py-4 font-semibold text-base text-gray-700">
+                      <td className="px-6 py-4 font-semibold text-base text-[#666666]">
                         {row.date}
                       </td>
-                      <td className="px-6 py-4 font-semibold text-base text-gray-700">
-                        <span className={`text-yellow-700`}>{row.status}</span>
-                      </td>
-                      <td className="px-6 py-4 font-semibold text-base text-gray-700">
+                      <td className="px-6 py-4 font-semibold text-base text-[#666666]">
+                      <span className={`text-[#F29509]`}>{row.status}</span>
+                    </td> 
+                      <td className="px-6 py-4 font-semibold text-base text-[#666666]">
                         {row.creditScore}
                       </td>
-                      <td className="px-6 py-4 font-semibold text-base text-gray-700">
+                      <td className="px-6 py-4 font-semibold text-base text-[#666666]">
                         {row.amount}
                       </td>
                     </tr>
@@ -99,7 +102,6 @@ const Pending = () => {
     </div>
   );
 };
-
 export default Pending;
 
 
--- omega-client/src/features/Dashboard/Dashboard/loans/Refunded.jsx
+++ omega-client/src/features/Dashboard/Dashboard/loans/Refunded.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import DashHeader from "../../../../components/Dashboard/DashHeader";
 import Sidebar from "../../../../components/Dashboard/Sidebar";
-import { TfiAngleDown } from 'react-icons/tfi';
+import { TfiAngleDown } from "react-icons/tfi";
 import { Link } from "react-router-dom";
 
 const Refunded = () => {
@@ -27,61 +27,45 @@ const Refunded = () => {
       <DashHeader />
       <div className="flex relative">
         <Sidebar />
-
         <div className="lg:w-[938px] lg:h-[fit] bg-[#FAFCFF] border border-[#CCE1FF] ml-[44px] mt-[128px] mb-16">
           <div className="flex justify-between">
-          <div className="mt-5 p-5">
-            <p className="font-semibold text-xl text-[#013E99] ">Loans Refunded</p>
-            <p className="font-medium text-base text-[#04AB33] mt-5">Total Amount: 4,587,541.28</p>
-            <p className="font-semibold text-base text-[#0252CC] mt-5">Last Month</p>
-          </div>
-          <div className="flex mt-10 p-10 mr-10 gap-2 items-center">
-          <p className="text-[#0252CC]">Sort by</p>
-            <TfiAngleDown />
-            <p className="text-[#4D4D4D]">Month</p>
-          </div>
-          </div>
-          <div className="lg:w-[938px] lg:h-[fit] bg-[#FAFCFF] border border-[#CCE1FF] mt-[40px] mb-16 ">
-            <div className="flex justify-between">
-              <div className="mt-5 p-5">
-                <p className="font-semibold text-xl text-blue-800 ">
-                  Loans Refunded
-                </p>
-                <p className="font-medium text-base text-green-500 mt-5">
-                  Total Amount: 4,587,541.28
-                </p>
-                <p className="font-semibold text-base text-blue-700 mt-5">
-                  Last Month
-                </p>
-              </div>
-              <div className="flex mt-10 p-10 mr-10 gap-2 items-center">
-                <p className="text-blue-500">Sort by</p>
-                <TfiAngleDown />
-                <p className="text-gray-700">Month</p>
-              </div>
+            <div className="mt-5 p-5">
+              <p className="font-semibold text-xl text-[#013E99] ">
+                Loans Generated
+              </p>
+              <p className="font-medium text-base text-[#0267FF] mt-5">
+                Total Amount: 4,587,541.28
+              </p>
+              <p className="font-semibold text-base text-[#0252CC] mt-5">
+                Last Month
+              </p>
             </div>
-
-          <div className="overflow-x-auto flex items-center justify-center">
-            <table className="min-w-full table-fixed border-collapse">
-              <thead >
-                <tr>
-                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">Borrower's Name</th>
-                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">Date</th>
-                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">Status</th>
-                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">Credit Score</th>
-                  <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">Amount</th>
-                </tr>
-              </thead>
-              <tbody>
-                {repeatData.map((row, index) => (
-                  <tr key={index} className={index % 2 === 0 ? 'bg-gray-50' : 'bg-white'}>
-                    <td className="px-6 py-4 font-semibold text-base text-[#666666]">{row.name}</td>
-                    <td className="px-6 py-4 font-semibold text-base text-[#666666]">{row.date}</td>
-                    <td className="px-6 py-4 font-semibold text-base text-[#666666]">
-                      <span className={`text-[#04AB33]`}>{row.status}</span>
-                    </td>
-                    <td className="px-6 py-4 font-semibold text-base text-[#666666]">{row.creditScore}</td>
-                    <td className="px-6 py-4 font-semibold text-base text-[#666666]">{row.amount}</td>
+            <div className="flex mt-10 p-10 mr-10 gap-2 items-center">
+              <p className="text-[#0252CC]">Sort by</p>
+              <TfiAngleDown />
+              <p className="text-[#4D4D4D]">Month</p>
+            </div>
+          </div>
+          <div className="lg:w-[938px] lg:h-[fit] bg-[#FAFCFF] border border-[#CCE1FF] mt-[40px] mb-16">
+            <div className="overflow-x-auto flex items-center justify-center">
+              <table className="min-w-full table-fixed border-collapse">
+                <thead>
+                  <tr>
+                    <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">
+                      Borrower's Name
+                    </th>
+                    <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">
+                      Date
+                    </th>
+                    <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">
+                      Status
+                    </th>
+                    <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">
+                      Credit Score
+                    </th>
+                    <th className="px-6 py-3 bg-blue-100 text-left font-semibold text-[#666666]">
+                      Amount
+                    </th>
                   </tr>
                 </thead>
                 <tbody>
@@ -90,19 +74,19 @@ const Refunded = () => {
                       key={index}
                       className={index % 2 === 0 ? "bg-gray-50" : "bg-white"}
                     >
-                      <td className="px-6 py-4 font-semibold text-base text-gray-700">
+                      <td className="px-6 py-4 font-semibold text-base text-[#666666]">
                         {row.name}
                       </td>
-                      <td className="px-6 py-4 font-semibold text-base text-gray-700">
+                      <td className="px-6 py-4 font-semibold text-base text-[#666666]">
                         {row.date}
                       </td>
-                      <td className="px-6 py-4 font-semibold text-base text-gray-700">
-                        <span className={`text-green-700`}>{row.status}</span>
+                      <td className="px-6 py-4 font-semibold text-base text-[#666666]">
+                        <span className={`text-[#04AB33]`}>{row.status}</span>
                       </td>
-                      <td className="px-6 py-4 font-semibold text-base text-gray-700">
+                      <td className="px-6 py-4 font-semibold text-base text-[#666666]">
                         {row.creditScore}
                       </td>
-                      <td className="px-6 py-4 font-semibold text-base text-gray-700">
+                      <td className="px-6 py-4 font-semibold text-base text-[#666666]">
                         {row.amount}
                       </td>
                     </tr>
@@ -118,7 +102,3 @@ const Refunded = () => {
 };
 
 export default Refunded;
-
-
-
-
