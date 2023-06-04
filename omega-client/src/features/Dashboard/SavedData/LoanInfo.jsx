f0007c1dc6fb75367a6abf3cf6a986e1a1e7ae69
Ketcha KAREN <60319455+leonelta@users.noreply.github.com>
2023-06-04 23:01:32 +0100

    Update LoanInfo.jsx

--- omega-client/src/features/Dashboard/SavedData/LoanInfo.jsx
+++ omega-client/src/features/Dashboard/SavedData/LoanInfo.jsx
@@ -1,62 +1,40 @@
-import React from 'react'
-import DashHeader from '../../../components/Dashboard/DashHeader'
-import Sidebar from '../../../components/Dashboard/Sidebar'
+mport React from 'react'
 
 const LoanInfo = () => {
   return (
-    <div className="flex flex-col">
-      <DashHeader />
-      <div className="flex relative">
-          <Sidebar />
-          <div className='flex flex-col ml-[80px] absolute top-[112px] left-[300px]'>
-            <div className="mt-[50px] text-[24px] font-[600] text-[#0267FF]">
-              Borrower’s Saved Data
-            </div>
-            <div className="mt-[19px] text-[18px] font-[600] text-[#4D4D4D]">
-              Loan Information
-            </div>
-            <div className="flex gap-[153px] mt-[58px]">
-                <div className="flex flex-col gap-[24px]">
-                  <input
-                    type="text"
-                    className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
-                    placeholder="Student Loan"
-                  />
-                  <input
-                    type="text"
-                    className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
-                    placeholder="Input Credit report"
-                  />
-                </div>
-                <div className="flex flex-col gap-[24px] ">
-                  <input
-                    type="text"
-                    className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
-                    placeholder="Automatic transfer"
-                  />
-                  <input
-                    type="text"
-                    className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
-                    placeholder="450"
-                  />
-                </div>
-            </div>
-            <div className='px-8 mt-12'>
-                <textarea
-                    className='bg-[#EEF5FC] border border-[#0267FF] w-[983px] h-[134px] px-4 py-4 rounded'
-                    placeholder='For payment of school fees'
-                ></textarea>
-            </div>
-            <div className="flex gap-[153px] mt-[58px]">
-                <div className="flex flex-col gap-[24px]">
-                    <button>Previous</button>
-                </div>
-                <div className="flex flex-col gap-[24px] ">
-                    <button>Next</button>
-                </div>
-            </div>
+    <div className='flex flex-col ml-[80px] absolute top-[112px] left-[300px]'>
+       <div className="flex gap-[153px] mt-[58px]">
+          <div className="flex flex-col gap-[24px]">
+            <input
+              type="text"
+              className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
+              placeholder="Student Loan"
+            />
+            <input
+              type="text"
+              className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
+              placeholder="Input Credit report"
+            />
+          </div>
+          <div className="flex flex-col gap-[24px] ">
+            <input
+              type="text"
+              className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
+              placeholder="Automatic transfer"
+            />
+            <input
+              type="text"
+              className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
+              placeholder="350"
+            />
+          </div>
         </div>
-      </div>
+        <div className='px-8 mt-12'>
+          <textarea
+            className='bg-[#EEF5FC] border border-[#0267FF] w-[983px] h-[134px] px-4 py-4 rounded'
+            placeholder='For payment of school fees'
+          ></textarea>
+        </div>
     </div>
   )
 }
