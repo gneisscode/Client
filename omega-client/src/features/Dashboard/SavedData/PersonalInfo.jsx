d3c80d4cfe19a39cceaa6851af7d8dd8242fbb00
unknown <ketchakaren@gmail.com>
2023-06-05 09:25:39 +0100

    added all files corcerning the borrowers saved data

---
+++ omega-client/src/features/Dashboard/SavedData/PersonalInfo.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const PersonalInfo = () => {
+  return (
+   <>
+    <div className="flex gap-[153px] mt-[58px]">
+        <div className="flex flex-col gap-[24px]">
+          <input
+            type="text"
+            className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
+            placeholder="Mr. Karim  Tunde "
+          />
+          <input
+            type="text"
+            className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
+            placeholder="kartun@gmail.com"
+          />
+          <input
+            type="text"
+            className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
+            placeholder="45 cresent Ikeja"
+          />
+          <input
+            type="text"
+            className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
+            placeholder="Unemployed"
+          />
+        </div>
+        <div className="flex flex-col gap-[24px] ">
+          <input
+            type="text"
+            className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
+            placeholder="+234658767520"
+          />
+          <input
+            type="text"
+            className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
+            placeholder="13th August 1990"
+          />
+          <input
+            type="text"
+            className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
+            placeholder="********"
+          />
+          <input
+            type="text"
+            className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
+            placeholder="10,000 Naira"
+          />
+        </div>
+      </div>
+   </>
+  );
+};
+
+export default PersonalInfo;
--- omega-client/src/features/Dashboard/SavedData/SavedData.jsx
+++ omega-client/src/features/Dashboard/SavedData/SavedData.jsx
@@ -1,70 +1,94 @@
-import React from 'react'
+import React, { useState } from 'react'
 import DashHeader from '../../../components/Dashboard/DashHeader'
 import Sidebar from '../../../components/Dashboard/Sidebar'
+import Circle from '../../../components/Circle/circle'
+import Button from '../../../components/Button'
+import PersonalInfo from './PersonalInfo'
+import LoanInfo from './LoanInfo'
+import Collateral from './Collateral'
+import Guarantors from './Guarantors'
+
 const SavedData = () => {
+  const slides = [0, 1, 2, 3] 
+  const [activeIndex, setActiveIndex] = useState
+
+  const steps = {
+    0: {
+      id: 0,
+      title: 'Personal and contact Information',
+      form: <PersonalInfo />,
+    },
+    1: {
+      id: 1,
+      title: 'Loan Information',
+      form: <LoanInfo />,
+    },
+    2: {
+      id: 2,
+      title: 'Collateral Information',
+      form: <Collateral />,
+    },
+    3: {
+      id: 3,
+      title: 'Guarantor’s Information',
+      form: <Guarantors />,
+    },
+  } 
+
+  const step = steps[activeIndex] 
   return (
-    <div className="flex flex-col">
+    <div className='flex flex-col'>
       <DashHeader />
-      <div className="flex ">
+      <div className='flex relative '>
         <Sidebar />
-        <div className='flex flex-col ml-[80px]'>
-          <div className="mt-[50px] text-[24px] font-[600] text-[#0267FF]">
-            Borrower’s Saved Data{" "}
-          </div>
-          <div className="mt-[19px] text-[18px] font-[600] text-[#666666]">
-            Personal and contact Information
-          </div>
-          <div className="flex gap-[153px] mt-[58px]">
-            <div className="flex flex-col gap-[24px]">
-              <input
-                type="text"
-                className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
-                placeholder="Full Name"
-              />
-              <input
-                type="text"
-                className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
-                placeholder="Email"
-              />
-              <input
-                type="text"
-                className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
-                placeholder="Address"
-              />
-              <input
-                type="text"
-                className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
-                placeholder="Employment Status"
-              />
+        <section>
+          <div>
+            <div className='flex flex-col gap-[16px]'>
+              <h3 className='text-[#0267FF] text-[24px] font-[600]'>
+                Borrower's Saved Data
+              </h3>
+              <h4 className='text-[#4D4D4D] text-[20px] font-[600]'>
+                {step.title}
+              </h4>
             </div>
-            <div className="flex flex-col gap-[24px] ">
-              <input
-                type="text"
-                className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
-                placeholder="Phone Number"
-              />
-              <input
-                type="text"
-                className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
-                placeholder="Date Of Birth"
-              />
-              <input
-                type="text"
-                className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
-                placeholder="Password"
-              />
-              <input
-                type="text"
-                className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
-                placeholder="Loan Amount"
+            <div className='flex w-full mt-16'>
+              {step.form}
+              <div className='mt-8'></div>
+              <div className='bottom-10 absolute flex items-center justify-center w-full'>
+                <Circle 
+                  slides={slides}
+                  activeIndex={activeIndex}
+                  setActiveIndex={setActiveIndex}
+                />
+              </div>
+            </div>
+            <div className='grid grid-cols-2 justify-between items-center gap-20 mt-16 pb-[147px]'>
+              {activeIndex !== 0 ? (
+                <Button 
+                  className='bg-white text-[#0267FF] border border-[#0267FF] w-4/12'
+                  label='Previous'
+                  onClick={() => setActiveIndex((prev) => prev - 1)}
+                 />
+              ) : (
+                <div className='w-4/12'></div>
+              )}
+              <Button 
+                className={`text-white bg-[#0267FF] ${
+                  activeIndex === 0 ? 'w-4/12' : 'w-4/12'
+                }`}
+                label={activeIndex === 3 ? 'Save Data' : 'Next'}
+                onClick={() =>
+                  activeIndex === 2
+                    ? setActiveIndex((prev) => prev - 1)
+                    : setActiveIndex((prev) => prev + 1)
+                }
               />
             </div>
           </div>
-         <button>Next</button>
-        </div>
+        </section>
       </div>
     </div>
-  );
-}
+  ) 
+} 
 
-export default SavedData
+export default SavedData 
