fa433505a87ecd9f2e339586d09a888ca9d176a8
Ketcha KAREN <60319455+leonelta@users.noreply.github.com>
2023-06-05 09:29:16 +0100

    Update Collateral.jsx

---
+++ omega-client/src/features/Dashboard/SavedData/Collateral.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Collateral = () => {
+  return (
+    <>
+        <div className="flex flex-col ml-[80px] absolute top-[112px] left-[300px]">
+      <div className="flex gap-[153px] mt-[58px]">
+        <div className="flex flex-col gap-[24px]">
+          <input
+            type="text"
+            className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
+            placeholder="Cash equivalent"
+          />
+        </div>
+        <div className="flex flex-col gap-[24px] ">
+          <input
+            type="text"
+            className="border border-[#0252CC] rounded w-[400px] h-[60px] p-4"
+            placeholder="Lands"
+          />
+        </div>
+      </div>
+      <div className="px-8 mt-24">
+        <textarea
+          className="bg-[#EEF5FC] border border-[#0267FF] w-[983px] h-[237px] px-4 py-4 rounded"
+          placeholder="5 Buildings located in Abuja"
+        ></textarea>
+      </div>
+    </div>
+
+    </>  );
+};
+
+export default Collateral;
