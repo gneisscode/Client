2e75dc09794f4b13bc1a6cb9e90a02b336bb392a
gneisscode <103408439+gneisscode@users.noreply.github.com>
2023-06-06 20:55:12 +0200

    edited verification route

--- omega-client/src/features/Auth/ChangePassword/VerificationCode.jsx
+++ omega-client/src/features/Auth/ChangePassword/VerificationCode.jsx
@@ -1,18 +1,40 @@
 import LockIcon from '../../../components/LockIcon';
 import Card from '../../../components/Card';
 import PasswordBtn from '../../../components/PasswordBtn';
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from '../../../context/Context';
+import axios from 'axios';
 
 
 const VerificationCode = () => {
+  const { user } = useContext(Context);
+  
     const [otp, setOtp] = useState(['', '', '', '', '']);
+    const [formData, setFormData] = useState({
+      fiveDigitToken: "",
+    });
   
     const handleChange = (e, index) => {
       const newOtp = [...otp];
       newOtp[index] = e.target.value;
       setOtp(newOtp);
+      setFormData({fiveDigitToken: parseInt(otp.join(''))})
+      console.log(otp)
+      console.log(formData)
     };
+    const handleSubmit = async (event) => {
+      event.preventDefault()
+      try {
+        const response = await axios.post(
+          `/password-reset/${user.adminId}`, formData
+        );
+        console.log(response.data.data);
+        const data = response.data.data;
+      } catch (error) {
+        console.log(error);
+      }
+    }
   
   return (
     <div className="flex flex-col items-center gap-4 p-10">
@@ -21,28 +43,28 @@ const VerificationCode = () => {
       <p className="text-xl pb-4">Kindly enter the code sent to your mail</p>
       <div>
         <Card className="p-14 flex flex-col items-center gap-10 ">
-          <div className="flex justify-center mt-5">
-            {otp.map((digit, index) => (
-              <input
-                key={index}
-                className="w-12 h-12 text-3xl border border-blue-300 rounded-full mx-2 text-center"
-                type="text"
-                maxLength="1"
-                value={digit}
-                onChange={(e) => handleChange(e, index)}
-              />
-            ))}
-          </div>
-          <p className="p-6 mb-10">
-            Didn't get the code?{" "}
-            <a href="#" className="text-blue-600">
-              Resend
-            </a>
-          </p>
+          <form onSubmit={handleSubmit}>
+            <div className="flex justify-center mt-5">
+              {otp.map((digit, index) => (
+                <input
+                  key={index}
+                  className="w-12 h-12 text-3xl border border-blue-300 rounded-full mx-2 text-center"
+                  type="text"
+                  maxLength="1"
+                  value={digit}
+                  onChange={(e) => handleChange(e, index)}
+                />
+              ))}
+            </div>
+            <p className="p-6 mb-10">
+              Didn't get the code?{" "}
+              <a href="#" className="text-blue-600">
+                Resend
+              </a>
+            </p>
 
-          <Link to="/change-password">
             <PasswordBtn text="Verify" />
-          </Link>
+          </form>
         </Card>
       </div>
     </div>
