14f151e3678f0b35cac01229ca42601ed9746ab4
Marvelous <Marvelous@DESKTOP-51HEQCN.com>
2023-06-15 21:55:55 +0100

    Loaded values into borrowers saved data

--- omega-client/src/features/Dashboard/SavedData/Guarantors.jsx
+++ omega-client/src/features/Dashboard/SavedData/Guarantors.jsx
@@ -1,6 +1,31 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const Guarantors = () => {
+  const [guarantorData, setGuarantorData] = useState([])
+  const location = useLocation()
+  const id = location.pathname.split("/")[2]
+
+
+  function getLoanInfo(){
+    axios.get(`https://nodebtdev.onrender.com/api/loans?id=${id}`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    })
+    .then(response => {
+      setGuarantorData(response.data.data.borrower.guarantor)
+  })
+  .catch(error => {
+    console.log(error);
+  })
+  }
+  useEffect(() => {
+    getLoanInfo()
+  }, [])
   return (
     <>
       <div className="flex flex-col gap-[16px] absolute top-[-100px] ">
@@ -16,26 +41,31 @@ const Guarantors = () => {
           <input
             type="text"
             className="border border-[#0252CC] rounded w-[300px] h-[60px] p-4"
+            value={guarantorData.fullname}
             placeholder="Mr. Hakim Tunde"
           />
           <input
             type="text"
             className="border border-[#0252CC] rounded w-[300px] h-[60px] p-4"
+            value={guarantorData.email}
             placeholder="Hatun@gmail.com"
           />
           <input
             type="text"
             className="border border-[#0252CC] rounded w-[300px] h-[60px] p-4"
+            value={guarantorData.address}
             placeholder="45 cresent Ikeja"
           />
           <input
             type="text"
             className="border border-[#0252CC] rounded w-[300px] h-[60px] p-4"
+            value={guarantorData.relationship}
             placeholder="Brother-in-law"
           />
           <input
             type="text"
             className="border border-[#0252CC] rounded w-[300px] h-[60px] p-4"
+            value={guarantorData.otherSourcesOfIncome}
             placeholder="stocks, Real Estate"
           />
         </div>
@@ -43,27 +73,32 @@ const Guarantors = () => {
           <input
             type="text"
             className="border border-[#0252CC] rounded w-[300px] h-[60px] p-4"
+            value={guarantorData.phoneNumber}
             placeholder="+234675892364"
           />
           <input
             type="text"
             className="border border-[#0252CC] rounded w-[300px] h-[60px] p-4"
             placeholder="20th December 1968"
+            value={guarantorData.age}
           />
           <input
             type="text"
             className="border border-[#0252CC] rounded w-[300px] h-[60px] p-4"
             placeholder="Social Security Number"
+            value={guarantorData.socialSecurityNumber}
           />
           <input
             type="text"
             className="border border-[#0252CC] rounded w-[300px] h-[60px] p-4"
             placeholder="Accountant"
+            value={guarantorData.employmentType}
           />
           <input
             type="text"
             className="border border-[#0252CC] rounded w-[300px] h-[60px] p-4"
             placeholder="600,000 Naira"
+            value={guarantorData.incomePerMonth}
           />
         </div>
       </div>
