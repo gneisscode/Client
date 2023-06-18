736970e9adcca94175d53849381d3f342606aad7
StephOge <nwachukwustephenie0@gmail.com>
2023-06-18 14:15:13 +0100

    searchbar

--- omega-client/src/components/Dashboard/DashHeader.jsx
+++ omega-client/src/components/Dashboard/DashHeader.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../../context/Context";
 import axios from "axios";
@@ -13,9 +13,9 @@ const DashHeader = () => {
     setSearchValue(event.target.value);
   };
 
-  const handleSearchSubmit = async (event) => {
-    event.preventDefault();
+  const fetchSearchResults = async () => {
     try {
+      const encodedSearchValue = encodeURIComponent(searchValue);
       const response = await axios.get(
         `https://nodebtdev.onrender.com/api/loans/getloan/${encodeURIComponent(searchValue)}`
       );
@@ -26,6 +26,14 @@ const DashHeader = () => {
     }
   };
 
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      fetchSearchResults();
+    }, 500); 
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchValue]);
+
 
   return (
     <div className="hidden lg:flex justify-between h-[112px] w-[100%] bg-[#FAFCFF] text-gray-400 border border-gray-100 items-end px-[80.11px] py-8 fixed top-0 left-0 z-[1000]">
@@ -35,7 +43,7 @@ const DashHeader = () => {
         </div>
       </Link>
 
-      <form onSubmit={handleSearchSubmit} className="relative">
+      <form className="relative">
         <input
           type="text"
           placeholder="Search"
@@ -43,11 +51,6 @@ const DashHeader = () => {
           onChange={handleSearchChange}
           className="lg:w-[520px] lg:h-[48px] p-[24px] rounded-[8px] border border-[#E5E5E5] outline-none pr-[48px]"
         />
-        <button type="submit" className="absolute top-1/2 right-4 transform -translate-y-1/2">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-5 h-5">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 18l-6-6m0 0l6-6" />
-          </svg>
-        </button>
       </form>
 
       <div className="flex gap-[30px] justify-center items-center self-center">
